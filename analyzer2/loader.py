c249bcd95aa865e2f8f1318d7faa966280496898
Cyrus <cyhfung@gmail.com>
2024-07-03 21:44:56 -0400

    torch rnn update

--- analyzer2/loader.py
+++ analyzer2/loader.py
@@ -7,18 +7,30 @@ import emoji
 from nltk.tokenize import word_tokenize, wordpunct_tokenize
 # from keras_preprocessing.text import Tokenizer
 
+all_chars = string.ascii_letters + string.punctuation + string.digits
+n_chars = len(all_chars)
 
-# can also use demoji package
-# demoji.download_codes()
-
-class Message():
+class Message(str):
     def __init__(self, message):
+        super(Message, self).__init__()
         self.message = message
-        self.all_chars = string.ascii_letters
-        self.n_chars = len(self.all_chars)
 
     def extract_emojis(self):
-        return emoji.demojize(self.message, delimiters=("", ""))
+        return Message(emoji.demojize(self.message, delimiters=("", "")))
+
+    def char_embedding(self):
+        """Embedding of each letter using a tensor/array as a one-hot vector"""
+        tensor = torch.zeros(len(self.message), 1, n_chars)
+        for num, letter in enumerate(self.message):
+            ind = all_chars.find(letter)
+            if ind != -1:
+                tensor[num][0][ind] = 1
+        return tensor
+
+    def embed(self):
+        self.message = self.extract_emojis()
+        self.message = self.char_embedding()
+        return self.message
 
     def word_embedding(self):
         pass
@@ -30,29 +42,29 @@ class Message():
         vocab = set(message)
         word_to_ix = {word: i for i, word in enumerate(vocab)}
         ngrams = [([message[i-j-1] for j in range(context_size)], message[i]) for i in range(context_size, len(message))]
-
-    def char_embedding(self):
-        """Embedding of each letter using a tensor/array as a one-hot vector"""
-        tensor = torch.zeros(len(self.message), 1, self.n_chars)
-        for num, letter in enumerate(self.message):
-            tensor[num][0][self.all_chars.find(letter)] = 1
-        return tensor
-
-class Dataset():
-    def __init__(self, filename: str, filetype: str):
-        self.data = None
-        if filetype == "json":
-            with open(f"../data/{filename}") as f:
-                self.data = json.load(f)
-        elif filetype == "csv":
-            posts = pd.read_csv(f"../data/{filename}")
-            # posts.drop(["Unnamed: 0"], axis=1, inplace=True)
-            # posts[["class"]] = (posts[["class"]] == "suicide").astype("int16")
-
-    def print_data(self):
         pass
 
+class Dataset():
+    def __init__(self, filename: str, filetype: str = "csv"):
+        self.data = None
+        if filetype == "csv":
+            self.data = pd.read_csv(f"../data/{filename}")
+            self.data.drop(["Unnamed: 0"], axis=1, inplace=True)
+            self.data[["class"]] = (self.data[["class"]] == "suicide").astype("int16")
+        else: #filetype == "json":
+            with open(f"../data/{filename}") as f:
+                self.data = json.load(f)
+        self.data.insert(1, "tensors", [Message(self.data.iloc[i, 0]).embed() for i in range(self.data.shape[0])], allow_duplicates=True)
+        print(self.data.head())
+
+    def print_data(self):
+        if self.data is not None:
+            print(self.data.head())
+            return self.data
+        print("No data valid given")
 
 if __name__ == "__main__":
-    message = Message("hello how are you 😜").extract_emojis()
-    print(message)
+    data_obj = Dataset("Suicide_Detection.csv")
+    for i in range(5):
+        message = Message(data_obj.data.iloc[i, 0]).embed()
+        print(message.shape)
--- analyzer2/rnn.py
+++ analyzer2/rnn.py
@@ -4,6 +4,11 @@ from tqdm import tqdm
 import torch; torch.manual_seed(0)
 import torch.nn as nn
 import torch.nn.functional as F
+from torch.utils.data import TensorDataset, DataLoader
+from sklearn.model_selection import train_test_split
+# from sklearn.metrics import accuracy_score, roc_auc_score
+
+from loader import *
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
@@ -12,21 +17,71 @@ class RNN(nn.Module):
         super(RNN, self).__init__()
         self.hidden_size = hidden_size
 
-        self.i2h = nn.Linear(input_size, hidden_size)
-        self.h2h = nn.Linear(hidden_size, hidden_size)
-        self.h2o = nn.Linear(hidden_size, output_size)
+        self.i2h = nn.Linear(input_size + hidden_size, hidden_size)
+        self.i2o = nn.Linear(input_size + hidden_size, output_size)
+        # self.h2h = nn.Linear(hidden_size, hidden_size)
+        # self.h2o = nn.Linear(hidden_size, output_size)
 
-        # Activation functions
         self.softmax = nn.LogSoftmax(dim=1)
 
-        # self.N = torch.distributions.Normal(0, 1)
-        # self.N.loc = self.N.loc.cuda()
-        # self.N.scale = self.N.scale.cuda()
-
-        # Loss
-
     def forward(self, input, hidden):
-        hidden = F.tanh(self.i2h(input) + self.h2h(hidden))
-        output = self.h2o(hidden)
+        combination = torch.cat(self.i2h(input) + self.h2h(hidden))
+        hidden = self.i2h(combination)
+        output = self.i2o(combination)
         output = self.softmax(output)
-        return output
+        return output, hidden
+
+    def init_hidden(self):
+        return torch.zeros(1, self.hidden_size)
+
+def train(model, x, y, epochs, lr = 0.005, plot_loss=False):
+    bar = tqdm(total=epochs)
+    opt = torch.optim.SGD(model.parameters(), lr=lr)
+    nll = nn.NLLLoss()
+    all_losses = list()
+    for epoch in range(epochs):
+        bar.reset()
+        cumulative_loss = 0
+        opt.zero_grad()
+
+        output, hidden = None, None
+        for i in range(len(x)):
+            message = Message(x[i]).embed()
+            hidden = model.init_hidden()
+            for j, k in enumerate(message.size()[0]):
+                output, hidden = model(x, hidden)
+
+            loss = nll(output, y)
+            cumulative_loss += loss
+            loss.backward()
+            opt.step()
+
+            bar.set_description(f"Epoch {epoch+1}/{epochs} Loss {cumulative_loss:.4f}")
+            bar.update()
+        all_losses.append(cumulative_loss)
+    if plot_loss:
+        pass
+    return model
+
+def predict(input_message):
+    print(f'Predicting \"{input_message}\"')
+    # with torch.no_grad():
+    #     output = evaluate
+
+def get_category(output):
+    category_idx = torch.argmax(output)
+    return category_idx
+
+if __name__ == "__main__":
+    data_obj = Dataset("Suicide_Detection.csv")
+    n_hidden = 128
+    n_categories = 2    # 0 or 1
+
+    x, y = torch.Tensor(data_obj.data['tensors']).to(device), torch.Tensor(data_obj.data['class']).to(device)
+    # xtrain, xtest, ytrain, ytest = train_test_split(x, y, test_size=0.4, random_state = 12)
+    model = RNN(n_chars, n_hidden, n_categories)
+    model = train(model, x, y, epochs=500)
+
+    from pathlib import Path
+    Path("../models").mkdir(parents=True, exist_ok=True)
+    torch.save(model.state_dict(), '../models/torch_RNN.pth')
